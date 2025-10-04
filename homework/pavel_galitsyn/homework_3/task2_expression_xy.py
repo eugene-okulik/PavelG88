4f655cc2f59908cfe9391a93073269ba549f8e79
PavelGN <Pavelwower@gmail.com>
2025-10-04 14:47:26 +0500

    Homework3 files

---
+++ homework/pavel_galitsyn/homework_3/task2_expression_xy.py
@@ -0,0 +1,5 @@
+x = 14
+y = 7
+
+result = x - y / 1 + x * y
+print("Результат:", result)
---
+++ homework/pavel_galitsyn/homework_3/task3_arithmetic_geometric.py
@@ -0,0 +1,11 @@
+import math
+
+a = 17
+b = 3
+
+srednee_arif = (a + b) / 2
+
+srednee_geom = math.sqrt(a * b)
+
+print("Среднее арифметическое:", srednee_arif)
+print("Среднее геометрическое:", srednee_geom)
