c74550573a7677957a9ee20f86e051d013e98d45
PavelGN <Pavelwower@gmail.com>
2025-11-06 11:22:23 +0500

    IzmeneniyaKoda

--- homework/pavel_galitsyn/homework_16/Bd&csv.py
+++ homework/pavel_galitsyn/homework_16/Bd&csv.py
@@ -1,35 +1,71 @@
 import csv
-import mysql.connector as mysql
 import os
+
 import dotenv
+import mysql.connector as mysql
 
 dotenv.load_dotenv()
 
 db = mysql.connect(
-    user=os.getenv('DB_USER'),
-    passwd=os.getenv('DB_PASSW'),
-    host=os.getenv('DB_HOST'),
-    port=os.getenv('DB_PORT'),
-    database=os.getenv('DB_NAME')
+    user=os.getenv("DB_USER"),
+    passwd=os.getenv("DB_PASSW"),
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    database=os.getenv("DB_NAME")
 )
 
 cursor = db.cursor(dictionary=True)
 
-with open(
-    r'D:\Work\GitProject\PavelG88\homework\eugene_okulik\Lesson_16\data.csv',
-    newline='',
-    encoding='utf-8-sig'
-) as csv_file:
+base_dir = os.path.dirname(__file__)
+csv_path = os.path.join(
+    base_dir, "..", "..", "eugene_okulik", "Lesson_16", "hw_data", "data.csv"
+)
+csv_path = os.path.abspath(csv_path)
+
+with open(csv_path, newline="", encoding="utf-8-sig") as csv_file:
     file_data = csv.reader(csv_file)
     not_found = []
 
     for row in file_data:
-        last, name, city = row
+        (
+            name,
+            second_name,
+            group_title,
+            book_title,
+            subject_title,
+            lesson_title,
+            mark_value
+        ) = row
+
         query = """
-        SELECT * FROM students
-        WHERE second_name = %s AND name = %s
+        SELECT m.id
+        FROM marks m
+        JOIN students s ON m.student_id = s.id
+        JOIN lessons l ON m.lesson_id = l.id
+        JOIN subjects sub ON l.subject_id = sub.id
+        JOIN books b ON b.taken_by_student_id = s.id
+        JOIN `groups` g ON s.group_id = g.id
+        WHERE s.name = %s
+          AND s.second_name = %s
+          AND g.title = %s
+          AND b.title = %s
+          AND sub.title = %s
+          AND l.title = %s
+          AND m.value = %s
         """
-        cursor.execute(query, (last, name))
+
+        cursor.execute(
+            query,
+            (
+                name,
+                second_name,
+                group_title,
+                book_title,
+                subject_title,
+                lesson_title,
+                mark_value
+            )
+        )
         result = cursor.fetchall()
 
         if not result:
