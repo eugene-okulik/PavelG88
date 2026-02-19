6038afb40e44e2659a56c7d3dc660323ee87490b
PavelGN <Pavelwower@gmail.com>
2026-02-19 18:10:52 +0500

    linterfix

--- homework/pavel_galitsyn/homework_28/test_playwright_task3.py
+++ homework/pavel_galitsyn/homework_28/test_playwright_task3.py
@@ -5,7 +5,7 @@ from playwright.sync_api import Page, expect
 
 def test_dynamic(page: Page):
     page.goto("https://demoqa.com/")
-    page.get_by_role('heading', name= 'Elements').click()
+    page.get_by_role('heading', name='Elements').click()
     page.get_by_text('Dynamic Properties').click()
 
     page.wait_for_load_state("networkidle")
